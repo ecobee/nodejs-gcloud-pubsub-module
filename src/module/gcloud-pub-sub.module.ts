028225e310ae33da7989142e21af2154a64c815c
Arvin Singla <arvin.singla@gmail.com>
2019-11-22 15:58:02 -0500

    Adding default publishOptions if none are sent.

--- src/module/gcloud-pub-sub.module.ts
+++ src/module/gcloud-pub-sub.module.ts
@@ -1,5 +1,7 @@
 import { DynamicModule, Module, Provider } from '@nestjs/common'
+import { PublishOptions } from '@google-cloud/pubsub/build/src/topic'
 import { GcloudPubSubService } from './gcloud-pub-sub.service'
+import { PUB_SUB_DEFAULT_BACKOFF_SETTINGS, PUB_SUB_DEFAULT_RETRY_CODES } from '../helpers/constants'
 import {
 	GcloudPubSubModuleAsyncOptions,
 	GcloudPubSubModuleOptions,
@@ -7,6 +9,17 @@ import {
 } from '../interfaces/gcloud-pub-sub.interface'
 
 const GCLOUDPUBSUB_MODULE_OPTIONS = 'GcloudPubSubModuleOptions'
+
+const defaultPublishOptions: PublishOptions = {
+	gaxOpts: {
+		retry: {
+			retryCodes: PUB_SUB_DEFAULT_RETRY_CODES,
+			backoffSettings: {
+				...PUB_SUB_DEFAULT_BACKOFF_SETTINGS,
+			},
+		},
+	},
+}
 
 const createAsyncProviders = (options: GcloudPubSubModuleAsyncOptions): Provider[] => {
 	if (options.useExisting || options.useFactory) {
@@ -40,11 +53,12 @@ const createAsyncOptionsProvider = (options: GcloudPubSubModuleAsyncOptions): Pr
 @Module({})
 export class GcloudPubSubModule {
 	static forRoot(moduleOptions: GcloudPubSubModuleOptions): DynamicModule {
+		const { authOptions, publishOptions } = moduleOptions
 		const providers = [
 			{
 				provide: GcloudPubSubService,
 				useFactory: async (): Promise<GcloudPubSubService> =>
-					new GcloudPubSubService(moduleOptions.authOptions, moduleOptions.publishOptions),
+					new GcloudPubSubService(authOptions, publishOptions || defaultPublishOptions),
 			},
 		]
 		return {
@@ -62,11 +76,10 @@ export class GcloudPubSubModule {
 				provide: GcloudPubSubService,
 				useFactory: async (
 					gcloudPubSubModuleOptions: GcloudPubSubModuleOptions
-				): Promise<GcloudPubSubService> =>
-					new GcloudPubSubService(
-						gcloudPubSubModuleOptions.authOptions,
-						gcloudPubSubModuleOptions.publishOptions
-					),
+				): Promise<GcloudPubSubService> => {
+					const { authOptions, publishOptions } = gcloudPubSubModuleOptions
+					return new GcloudPubSubService(authOptions, publishOptions || defaultPublishOptions)
+				},
 				inject: ['GcloudPubSubModuleOptions'],
 			},
 		]
