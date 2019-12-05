2b0917de064c3c40dda5fe9e339c12fd3cd99174
Arvin Singla <arvin.singla@gmail.com>
2019-12-04 22:40:32 -0500

    Refactoring pubsub server error handler to be better handle reconnection on error.

--- src/microservice/gcloud-pub-sub.server.ts
+++ src/microservice/gcloud-pub-sub.server.ts
@@ -1,22 +1,21 @@
 import { PubSub, Subscription, Message } from '@google-cloud/pubsub'
 import { Server, CustomTransportStrategy } from '@nestjs/microservices'
 import { GCloudPubSubServerOptions } from '../interfaces/gcloud-pub-sub.interface'
-import { MESSAGE, ERROR, CLOSE } from '../helpers/constants'
+import { MESSAGE, ERROR, PUB_SUB_DEFAULT_RETRY_CODES } from '../helpers/constants'
 
-const NOT_FOUND_ERROR = 5
 const RETRY_INTERVAL = 5000
 
 export class GCloudPubSubServer extends Server implements CustomTransportStrategy {
 	public client: PubSub = null
 	public subscriptions: Subscription[] = []
-	public isClosing: boolean = false
+	public isShuttingDown: boolean = false
 
 	constructor(public readonly options: GCloudPubSubServerOptions) {
 		super()
 	}
 
 	public listen(callback: () => void) {
-		this.isClosing = false
+		this.isShuttingDown = false
 		this.client = new PubSub(this.options.authOptions)
 		this.options.subscriptionIds.forEach(subcriptionName => {
 			const subscription = this.client.subscription(
@@ -24,31 +23,30 @@ export class GCloudPubSubServer extends Server implements CustomTransportStrateg
 				this.options.subscriberOptions || {}
 			)
 			const handleMessage = this.handleMessageFactory(subcriptionName)
-			const handleError = this.handleErrorFactory(subscription)
+			const handleError = this.handleErrorFactory(subscription, subcriptionName)
 			subscription.on(MESSAGE, handleMessage.bind(this))
-			subscription.on(ERROR, handleError.bind(this))
-			subscription.on(CLOSE, this.close.bind(this))
+			subscription.on(ERROR, handleError)
 			this.subscriptions.push(subscription)
 		})
 		callback()
 	}
 
-	public handleErrorFactory(subscription: Subscription) {
+	public handleErrorFactory(subscription: Subscription, subcriptionName: string) {
 		return error => {
-			if (this.isClosing === false) {
+			this.handleError(error)
+			if (!this.isShuttingDown && PUB_SUB_DEFAULT_RETRY_CODES.includes(error.code)) {
+				this.logger.warn(`Closing subscription: ${subcriptionName}`)
 				subscription.close()
-				if (error.code === NOT_FOUND_ERROR) {
-					setTimeout(() => {
-						subscription.open()
-					}, RETRY_INTERVAL)
-				}
-				this.handleError(error)
+				setTimeout(() => {
+					this.logger.warn(`Opening subscription: ${subcriptionName}`)
+					subscription.open()
+				}, RETRY_INTERVAL)
 			}
 		}
 	}
 
 	public close() {
-		this.isClosing = true
+		this.isShuttingDown = true
 		this.subscriptions.forEach(subscription => {
 			subscription.close()
 		})
@@ -58,6 +56,7 @@ export class GCloudPubSubServer extends Server implements CustomTransportStrateg
 		return async (message: Message) => {
 			const handler = this.getHandlerByPattern(subscriptionName)
 			if (!handler) {
+				this.logger.warn(`ack message with no active handler: ${message.id}`)
 				message.ack()
 				return
 			}
